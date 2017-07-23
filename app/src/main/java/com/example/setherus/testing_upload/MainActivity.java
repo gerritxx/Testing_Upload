fb3c8d01eec5bf0ba02e5b663fe279861b6c0c79
Gerrit <gerritnaude@gmail.com>
2017-07-23 12:54:16 +0200

    Testing Test

---
+++ app/src/main/java/com/example/setherus/testing_upload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.setherus.testing_upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
