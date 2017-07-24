ce50b58bd9d9bcb0c462d1d79cc8c4281f08e742
Carl Meyer <carlmeyer1996@gmail.com>
2017-07-24 12:16:11 +0200

    Added method to btnNext that sets text on tvName to "Hello Word"

--- app/src/main/java/com/example/setherus/testing_upload/MainActivity.java
+++ app/src/main/java/com/example/setherus/testing_upload/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.setherus.testing_upload;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button = (Button)findViewById(R.id.btnNext);
+    TextView tvName = (TextView)findViewById(R.id.tvName);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void nextScreen(View view) {
+
+        tvName.setText(R.string.hello);
     }
 }
---
+++ app/src/main/java/com/example/setherus/testing_upload/SecondActivity.java
@@ -0,0 +1,8 @@
+package com.example.setherus.testing_upload;
+
+/**
+ * Created by Carl on 2017-07-24.
+ */
+
+public class SecondActivity {
+}
